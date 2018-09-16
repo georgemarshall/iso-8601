c5f446149d9f7a07caed293fe121c67499b69c20
Robin Stumm <serverkorken@gmail.com>
2018-09-16 23:26:01 +0200

    separate struct for each date type

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,35 @@
+#[macro_use] extern crate nom;
+
 mod parse;
 pub mod chrono;
-
-#[macro_use] extern crate nom;
 
 pub use parse::*;
 
 #[derive(Eq, PartialEq, Clone, Copy, Debug)]
 pub enum Date {
-    YMD {
-        year: i16,
-        month: u8,
-        day: u8
-    },
-    Week {
-        year: i16,
-        week: u8,
-        day: u8
-    },
-    Ordinal {
-        year: i16,
-        day: u16
-    }
+    YMD(YmdDate),
+    Week(WeekDate),
+    Ordinal(OrdinalDate)
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+pub struct YmdDate {
+    year: i16,
+    month: u8,
+    day: u8
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+pub struct WeekDate {
+    year: i16,
+    week: u8,
+    day: u8
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+pub struct OrdinalDate {
+    year: i16,
+    day: u16
 }
 
 #[derive(Eq, PartialEq, Clone, Copy, Debug)]
